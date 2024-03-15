0af83c5118f79d1761f4be161219fb5ac4019c1d
Manish bisht <manishsinghbisht10@gmail.com>
2024-03-15 16:43:53 +0530

    js functionality to movements

--- script.js
+++ script.js
@@ -61,6 +61,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovement = function (move) {
+  console.log(move);
+  containerMovements.innerHTML = '';
+  move.forEach(function (move, index) {
+    const type = move > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      index + 1
+    } ${type}</div>
+        <div class="movements__value">${move}</div>
+       </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovement(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
